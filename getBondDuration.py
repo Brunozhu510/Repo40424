e7e85b235d6e612e525cbc12ac3f0ffb1dbe024d
Brunozhu510 <165340075+Brunozhu510@users.noreply.github.com>
2024-04-10 19:29:04 -0400

    Create getBondDuration.py

---
+++ getBondDuration.py
@@ -0,0 +1,13 @@
+def getBondDuration(y, face, couponRate, m, ppy = 1):
+    <Your work>
+    return(bondDuration)
+
+
+
+# Test values
+
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
+ppy = 1
